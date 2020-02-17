37857980d3ae781f3b331c84d26fd7c52b41b7ab
Noppawat Chotiwan <noppawatchotiwan@gmail.com>
2020-02-17 23:37:05 +0700

    Add new signup component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from './pages/shop/Shop';
 import {Route,Switch} from 'react-router-dom';
 import Header from './component/header/Header';
 import SignInSignUp from './pages/sign-in-and-sign-up/Sign-in-Sign-up';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 function App() {
 
@@ -14,9 +14,34 @@ function App() {
   let unsubscribeFromAuth = null;
 
   useEffect(()=>{
-    unsubscribeFromAuth=  auth.onAuthStateChanged(user=>{
-        setCurrentUser({currentUser:user})
-        console.log(user)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    unsubscribeFromAuth=  auth.onAuthStateChanged( async userAuth=>{
+       if(userAuth){
+          const userRef= await createUserProfileDocument(userAuth);
+
+          userRef.onSnapshot(snapShot =>{
+
+            console.log('snapShot',snapShot);
+
+            setCurrentUser({
+              currentUser:{
+                id:snapShot.id,
+                ...snapShot.data()
+              }
+            })
+          })
+       }else{
+         setCurrentUser({currentUser:userAuth});
+       }
+       console.log('currentUser',currentUser.currentUser);
+
+      
+      
+      
+      
+      //setCurrentUser({currentUser:user})
+      //  console.log(user)
+      //  createUserProfileDocument(user);
       })
       //return unsubscribeFromAuth();
   }
---
+++ src/component/sign-up/SignUp.js
@@ -0,0 +1,113 @@
+import React,{useState} from 'react';
+
+import './sign-up.scss';
+
+import FormInput from '../form-input/FormInput';
+
+import CustomButton from '../custom-button/CustomButton';
+
+import {auth,createUserProfileDocument} from '../../firebase/firebase.utils';
+
+const SignUp = () =>{
+
+    const [input,setInput] = useState({
+        displayName:'',
+        email:'',
+        password:'',
+        confirmPassword:''
+    });
+
+    const {displayName,email,password,confirmPassword} = input;
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        const {displayName,email,password,confirmPassword} = input;
+        if(password!==confirmPassword){
+            alert("Passwords don't match");
+            return;
+        }
+        try{
+            const {user} = await auth.createUserWithEmailAndPassword(email,password);
+            await createUserProfileDocument(user,{displayName});
+
+            setInput('');
+
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    const handleChange = e =>{
+
+        const {name,value} = e.target;
+        
+        setInput({
+            [name] : value,
+            ...input
+        })
+    }
+
+    return(
+
+        <div className='sign-up'>
+            
+            <h2 className='title'>I do not have an account</h2>
+            
+            <span>Sign up with your email and password</span>
+            
+            <form className='sign-up-form' onSubmit={handleSubmit}>
+                <FormInput 
+                type='text' 
+                name='displayName' 
+                value={displayName} 
+                onChange={handleChange}
+                label='Display Name'
+                required
+                >
+                </FormInput>
+
+                <FormInput 
+                type='text' 
+                name='email' 
+                value={email} 
+                onChange={handleChange}
+                label='email'
+                required
+                >
+                </FormInput>
+
+                <FormInput 
+                type='pasword' 
+                name='password' 
+                value={password} 
+                onChange={handleChange}
+                label='Password'
+                required
+                >
+                </FormInput>
+
+                <FormInput 
+                type='password' 
+                name='confirmPassword' 
+                value={confirmPassword} 
+                onChange={handleChange}
+                label='Confirm Password'
+                required
+                >
+                </FormInput>
+
+                <CustomButton type='submit'>SignUp</CustomButton>
+            
+            </form>
+                
+        </div>    
+        
+        
+    )
+
+
+
+
+
+}
+export default SignUp;
