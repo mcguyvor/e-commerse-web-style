bfaf26e3592e5009a59b228139689ae7833de2c2
Noppawat Chotiwan <noppawatchotiwan@gmail.com>
2020-03-03 23:17:34 +0700

    add cart item

--- src/component/cart-dropdown/CartDropdown.js
+++ src/component/cart-dropdown/CartDropdown.js
@@ -4,11 +4,35 @@ import CustomButton from '../custom-button/CustomButton';
 
 import './cart-dropdown.scss';
 
-const CartDropdown = () =>(
-    <div className='cart-dropdown'>
-            <div className='cart-items'>
-                    <CustomButton>GO TO CHECKOUT</CustomButton>
-            </div>
-    </div>
-)
+import CartItem from '../cart-item/CartItem';
+
+import {useDispatch, useSelector} from 'react-redux';
+
+const CartDropdown = () =>{
+
+        const dispatch = useDispatch();
+
+        const cartItems = useSelector(state=> state.cart.cartItems);
+
+        return (
+
+                <div className='cart-dropdown'>
+            
+                        <div className='cart-items'>
+                        {
+                                cartItems.map(cartItem =>
+                                        <CartItem key={cartItem.id} item ={cartItem}/>
+                                        )
+                        }
+                        </div>
+                        
+                        <CustomButton>GO TO CHECKOUT</CustomButton>
+
+                </div>
+            );
+}
+
+
+
+
 export default CartDropdown;
---
+++ src/component/cart-item/CartItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './cart-item.scss';
+
+const CartItem = ({item :{imageUrl,price,name, quantity }})=>{
+
+    return(
+        <div className='cart-item'>
+            <img src={imageUrl} alt='alt'/>
+            <div className='item-details'>
+                <span className='name'>{name}</span>
+                <span className='price'>
+                    {quantity} x B{price}
+                    </span>
+            </div>
+        </div>
+    )
+}
+export default CartItem;
